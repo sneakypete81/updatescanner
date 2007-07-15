9138dc0acd654476dbd384aeb2c6d3d379823346
sneakypete81 <sneakypete81@gmail.com>
2007-07-15 17:28:50 +0000

    Added dutch translation

--- src/config.py
+++ src/config.py
@@ -7,15 +7,16 @@ description = 'Monitors webpages for updates'
 author = 'Pete Burgers'
 translators = ['SlovakSoft (Slovenčina)',
                'Almotasim (Italiano)',
-               'myahoo (Français)']
+               'myahoo (Français)',
+               'Mark Heijl (Nederlands)']
 authorURL = "http://updatescanner.mozdev.org"
 uid = 'c07d1a49-9894-49ff-a594-38960ede8fb9'
 
 major_version = 2
 minor_version = 0
-revision_version = 12
-build_version = False
-in_development = False
+revision_version = 13
+build_version = 1
+in_development = True
 
 if build_version:
     version = "%d.%d.%d.%d%s" % (
@@ -77,6 +78,10 @@ locales = {
     'fr-FR': {
         'locale_version': '1.0',
         'display_name': 'Français',
+    },
+    'nl-NL': {
+        'locale_version': '1.0',
+        'display_name': 'Nederlands',
     },
 }
 
