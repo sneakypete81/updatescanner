d53b9e8de4084d6afa81c49438fccb0d27767120
sneakypete81 <sneakypete81@gmail.com>
2008-02-25 22:00:04 +0000

    Adding simplified chinese

--- src/config.py
+++ src/config.py
@@ -19,7 +19,8 @@ translators = ['SlovakSoft (Slovenčina)',
                'Raryel Costa Souza (português brasileiro)',
                '123tp (Dansk)',
                'Kenan Balamir (Türkçe)',
-               'Wang King (Simplified Chinese)']
+               'Wang King (简化字 - Simplified Chinese)'
+               'Peter Pin-Guang Chen (簡化字 - Traditional Chinese]
 authorURL = "http://updatescanner.mozdev.org"
 uid = 'c07d1a49-9894-49ff-a594-38960ede8fb9'
 optionsChrome = 'chrome://updatescan/content/preferences.xul'
@@ -139,6 +140,10 @@ locales = {
         'display_name': name,
     },
     'zh-CN': {
+        'locale_version': '1.0',
+        'display_name': name,
+    },
+    'zh-TW': {
         'locale_version': '1.0',
         'display_name': name,
     },
