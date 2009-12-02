cbe71a0546aa1df81e5f3e9799d0be3a3bf99726
sneakypete81 <sneakypete81@gmail.com>
2009-12-02 18:52:46 +0000

    TODO & config updates

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding=UTF-8
 
-version = "3.0.7beta1" # Don't forget to update upgrade.js too!
-in_development = True
+version = "3.0.7alpha1" # Don't forget to update upgrade.js too!
+in_development = False
 publish_babelzilla = False # True = include incomplete locales for babelzilla
 only_english = True # True = only include english for beta releases
 
