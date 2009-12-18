98d4a1f55286f61e043a1b186c245dc68808ce77
sneakypete81 <sneakypete81@gmail.com>
2009-12-18 20:36:06 +0000

    Lets call it 3.1.0

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=UTF-8
 
-version = "3.0.7alpha1" # Don't forget to update upgrade.js too!
+version = "3.1.0alpha1" # Don't forget to update upgrade.js too!
 in_development = False
 publish_babelzilla = False # True = include incomplete locales for babelzilla
 only_english = True # True = only include english for beta releases
