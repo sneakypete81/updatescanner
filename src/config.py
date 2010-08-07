8e054103f59e398bd9935f394e668692f891e459
sneakypete81 <sneakypete81@gmail.com>
2010-08-07 16:48:51 +0100

    Update to 3.1.3beta1

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=UTF-8
 
-version = "3.1.2" # Don't forget to update upgrade.js too!
+version = "3.1.3beta1" # Don't forget to update upgrade.js too!
 in_development = False
 publish_babelzilla = False # True = include incomplete locales for babelzilla
 only_english = False # True = only include english for beta releases
@@ -81,7 +81,7 @@ locales = {
         'locale_version': '1.0',
         'display_name': name,
     },           
-    'de-DE': {
+    'de': {
         'locale_version': '1.0',
         'display_name': name,
     },           
