6793bb32f07e5ae28c34b0ac9e78d38c25b77fd3
sneakypete81 <sneakypete81@gmail.com>
2012-07-29 14:07:19 +0100

    Bump version to 3.1.10

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@
 
 import sys
 
-version = "3.1.10rc1" # Don't forget to update upgrade.js too!
+version = "3.1.10" # Don't forget to update upgrade.js too!
 publish_babelzilla = False # True = include incomplete locales for babelzilla
 only_english = False # True = only include english for beta releases
 
