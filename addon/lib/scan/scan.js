cc4716841f9a3c31e3a64acd1ade0a7f101e3c93
sneakypete81 <sneakypete81@gmail.com>
2017-01-05 19:14:09 +0000

    Finish Scan implementation and add tests

--- addon/lib/scan/scan.js
+++ addon/lib/scan/scan.js
@@ -1,10 +1,8 @@
 /* exported Scan */
-/* global PageStore */
-
-// Promise-XHR example from https://github.com/mdn/promises-test
+/* global Fuzzy, PageStore, Page */
 
 /**
- * Class used to scan a list of webpages.
+ * Class used to scan a list of webpages and test for updates.
  */
 class Scan {
   /**
@@ -15,11 +13,11 @@ class Scan {
   }
 
   /**
-   * Start scanning the pages one at a time. HTML is saved to the PageStore, and
-   * the Page objects are updated and saved accordingly.
+   * Start scanning the pages one at a time. HTML is checked for updates and
+   * saved to the PageStore, and the Page objects updated and saved accordingly.
    *
    * @returns {Promise} An empty promise that fulfills once all pages have been
-   * scanned.
+   * scanned and updated.
    */
   start() {
     // Construct a chain of Promises to scan each Page sequentially.
@@ -34,12 +32,14 @@ class Scan {
   }
 
   /**
-   * Scans a single page, saving the HTML to the PageStore and updating and
-   * saving the Page object accordingly.
+   * Scan a single page, check for updates, then save the HTML to the PageStore
+   * and updating and save the Page object accordingly. Errors are logged and
+   * ignored.
    *
    * @param {Page} page - Page to scan.
    *
-   * @returns {Promise} Pomise that fulfils once the page has been scanned.
+   * @returns {Promise} Promise that fulfils once the page has been scanned and
+   * updated.
    */
   static _scanPage(page) {
     // Function to check the status of a fetch response.
@@ -47,19 +47,119 @@ class Scan {
       if (response.ok) {
         return Promise.resolve(response);
       } else {
-        return Promise.reject(new Error(
-          '[' + response.status + '] ' + response.statusText));
+        return Promise.reject('[' + response.status + '] '
+                              + response.statusText);
       }
     };
 
     return fetch(page.url)
       .then(checkStatus)
       .then((response) => response.text())
-      .then((html) => {
-        console.log('HTML: ' + html.slice(0, 50));
-        PageStore.saveHtml(page.id, PageStore.htmlTypes.NEW, html);
-      }).catch((error) => {
+      .then((html) => Scan._processHtml(page, html))
+      .catch((error) => {
         console.log('Could not scan "' + page.title + '": ' + error);
+        page.error = true;
+        page.errorMessage = error;
       });
+  }
+
+  /**
+   * Load the "NEW" HTML from storage, compare it with the the scanned HTML,
+   * update the page state and update the HTML storage as necessary. returns
+   * without waiting for the save operations to complete.
+   * Note that the "NEW" HTML is used for comparison - this is the HTML that was
+   * downloaded during the most recent scan. This is the simplest and most
+   * resource-efficient approach.
+   *
+   * @param {Page} page - Page object to update.
+   * @param {string} scannedHtml - HTML to process.
+   */
+  static _processHtml(page, scannedHtml) {
+    PageStore.loadHtml(page.id, PageStore.htmlTypes.NEW).then((prevHtml) => {
+      Scan._updatePageState(page, prevHtml, scannedHtml);
+    });
+  }
+
+  /**
+   * Compare the scanned HTML with the "NEW" HTML from storage, update the page
+   * state and save the HTML to storage. The method returns without waiting for
+   * the save operations to complete.
+   *
+   * @param {Page} page - Page object to update.
+   * @param {string} prevHtml - HTML from storage.
+   * @param {string} scannedHtml - Scanned HTML to process.
+   */
+  static _updatePageState(page, prevHtml, scannedHtml) {
+    switch (Scan._getChangeType(prevHtml, scannedHtml, page.changeThreshold)) {
+      case Scan.changeEnum.NEW_CONTENT:
+      case Scan.changeEnum.MINOR_CHANGE:
+        PageStore.saveHtml(page.id, PageStore.htmlTypes.NEW, scannedHtml);
+        // Only update the state if not previously marked as changed.
+        if (page.state != Page.stateEnum.CHANGED) {
+          page.state = Page.stateEnum.NO_CHANGE;
+        }
+        break;
+
+      case Scan.changeEnum.MAJOR_CHANGE:
+        if (page.state != Page.stateEnum.CHANGED) {
+          // This is a newly detected change, so update the old HTML.
+          PageStore.saveHtml(page.id, PageStore.htmlTypes.OLD, prevHtml);
+        }
+        PageStore.saveHtml(page.id, PageStore.htmlTypes.NEW, scannedHtml);
+        page.state = Page.stateEnum.CHANGED;
+        break;
+
+      case Scan.changeEnum.NO_CHANGE:
+        // Only update the state if not previously marked as changed.
+        if (page.state != Page.stateEnum.CHANGED) {
+          page.state = Page.stateEnum.NO_CHANGE;
+        }
+        break;
+    }
+    page.error = false;
+    page.errorMessage = '';
+
+    // Commit changes, but don't wait for the save to complete
+    page.save();
+  }
+
+  /**
+   * Given two downloaded HTML strings, return a changeEnum value indicating how
+   * similar they are.
+   *
+   * @param {string} html1 - First HTML string for comparison.
+   * @param {string} html2 - Second HTML string for comparison.
+   * @param {integer} changeThreshold - Number of characters that must change to
+   * indicate a major change.
+   *
+   * @returns {string} Scan.changeEnum string indicating how similar the
+   * two HTML strings are.
+   */
+  static _getChangeType(html1, html2, changeThreshold) {
+    if (html1 == '') {
+      // This is the first scan.
+      return Scan.changeEnum.NEW_CONTENT;
+    } else if (html1 == html2) {
+      // HTML is unchanged.
+      return Scan.changeEnum.NO_CHANGE;
+    } else if (Fuzzy.isMajorChange(html1, html2, changeThreshold)) {
+      // Change is larger than changeThreshold.
+      return Scan.changeEnum.MAJOR_CHANGE;
+    } else {
+      // Change is smaller than changeThreshold.
+      return Scan.changeEnum.MINOR_CHANGE;
+    }
+  }
+
+  /**
+   * @returns {string} Enumeration indicating the similarity of two HTML
+   * strings.
+   */
+  static get changeEnum() {
+    return {NEW_CONTENT: 'new_content',
+            NO_CHANGE: 'no_change',
+            MAJOR_CHANGE: 'major_change',
+            MINOR_CHANGE: 'minor_change',
+          };
   }
 }
---
+++ test/unit/scan_spec.js
@@ -0,0 +1,259 @@
+/* global Scan, Fuzzy, Page, PageStore */
+
+describe('Scan', function() {
+  describe('_getChangeType', function() {
+    it('detects identical pages', function() {
+      const html = 'Here is some <b>HTML</b>';
+
+      const result = Scan._getChangeType(html, html, 100);
+
+      expect(result).toEqual(Scan.changeEnum.NO_CHANGE);
+    });
+
+    it('detects new content', function() {
+      const html = 'Here is some <b>HTML</b>';
+
+      const result = Scan._getChangeType('', html, 100);
+
+      expect(result).toEqual(Scan.changeEnum.NEW_CONTENT);
+    });
+
+    it('detects minor changes', function() {
+      const html1 = 'Here is some <b>HTML</b>';
+      const html2 = 'Here is some different <b>HTML</b>';
+      spyOn(Fuzzy, 'isMajorChange').and.returnValue(false);
+
+      const result = Scan._getChangeType(html1, html2, 100);
+
+      expect(result).toEqual(Scan.changeEnum.MINOR_CHANGE);
+    });
+
+    it('detects major changes', function() {
+      const html1 = 'Here is some <b>HTML</b>';
+      const html2 = 'Here is some different <b>HTML</b>';
+      spyOn(Fuzzy, 'isMajorChange').and.returnValue(true);
+
+      const result = Scan._getChangeType(html1, html2, 100);
+
+      expect(result).toEqual(Scan.changeEnum.MAJOR_CHANGE);
+    });
+  });
+
+  describe('_updatePageState', function() {
+    it('does nothing if the page is unchanged', function() {
+      const page = new Page('1', {changeThreshold: 100,
+                                  state: Page.stateEnum.NO_CHANGE});
+      const html = 'Here is some <b>HTML</b>';
+      spyOn(page, 'save');
+      spyOn(PageStore, 'saveHtml');
+
+      Scan._updatePageState(page, html, html);
+
+      expect(page.state).toEqual(Page.stateEnum.NO_CHANGE);
+      expect(PageStore.saveHtml).not.toHaveBeenCalled();
+    });
+
+    it('does nothing if the page is unchanged when previously changed',
+       function() {
+      const page = new Page('1', {changeThreshold: 100,
+                                  state: Page.stateEnum.CHANGED});
+      const html = 'Here is some <b>HTML</b>';
+      spyOn(page, 'save');
+      spyOn(PageStore, 'saveHtml');
+
+      Scan._updatePageState(page, html, html);
+
+      expect(page.state).toEqual(Page.stateEnum.CHANGED);
+      expect(PageStore.saveHtml).not.toHaveBeenCalled();
+    });
+
+    it('saves new content', function() {
+      const page = new Page('1', {changeThreshold: 100,
+                                  state: Page.stateEnum.NO_CHANGE});
+      const html = 'Here is some <b>HTML</b>';
+      spyOn(page, 'save');
+      spyOn(PageStore, 'saveHtml');
+
+      Scan._updatePageState(page, '', html);
+
+      expect(page.state).toEqual(Page.stateEnum.NO_CHANGE);
+      expect(PageStore.saveHtml).toHaveBeenCalledWith(
+        '1', PageStore.htmlTypes.NEW, html);
+      expect(PageStore.saveHtml).toHaveBeenCalledTimes(1);
+    });
+
+    it('saves a minor change without updating state', function() {
+      const page = new Page('1', {changeThreshold: 100,
+                                  state: Page.stateEnum.NO_CHANGE});
+      const html1 = 'Here is some <b>HTML</b>';
+      const html2 = 'Here is some different <b>HTML</b>';
+      spyOn(page, 'save');
+      spyOn(PageStore, 'saveHtml');
+      spyOn(Fuzzy, 'isMajorChange').and.returnValue(false);
+
+      Scan._updatePageState(page, html1, html2);
+
+      expect(page.state).toEqual(Page.stateEnum.NO_CHANGE);
+      expect(PageStore.saveHtml).toHaveBeenCalledWith(
+        '1', PageStore.htmlTypes.NEW, html2);
+      expect(PageStore.saveHtml).toHaveBeenCalledTimes(1);
+    });
+
+    it('doesn\'t update state for a minor change when previously changed',
+       function() {
+      const page = new Page('1', {changeThreshold: 100,
+                                  state: Page.stateEnum.CHANGED});
+      const html1 = 'Here is some <b>HTML</b>';
+      const html2 = 'Here is some different <b>HTML</b>';
+      spyOn(page, 'save');
+      spyOn(PageStore, 'saveHtml');
+      spyOn(Fuzzy, 'isMajorChange').and.returnValue(false);
+
+      Scan._updatePageState(page, html1, html2);
+
+      expect(page.state).toEqual(Page.stateEnum.CHANGED);
+      expect(PageStore.saveHtml).toHaveBeenCalledWith(
+        '1', PageStore.htmlTypes.NEW, html2);
+      expect(PageStore.saveHtml).toHaveBeenCalledTimes(1);
+    });
+
+    it('updates old and new HTML for a new major change', function() {
+      const page = new Page('1', {changeThreshold: 100,
+                                  state: Page.stateEnum.NO_CHANGE});
+      const html1 = 'Here is some <b>HTML</b>';
+      const html2 = 'Here is some different <b>HTML</b>';
+      spyOn(page, 'save');
+      spyOn(PageStore, 'saveHtml');
+      spyOn(Fuzzy, 'isMajorChange').and.returnValue(true);
+
+      Scan._updatePageState(page, html1, html2);
+
+      expect(page.state).toEqual(Page.stateEnum.CHANGED);
+      expect(PageStore.saveHtml).toHaveBeenCalledWith(
+        '1', PageStore.htmlTypes.OLD, html1);
+      expect(PageStore.saveHtml).toHaveBeenCalledWith(
+        '1', PageStore.htmlTypes.NEW, html2);
+      expect(PageStore.saveHtml).toHaveBeenCalledTimes(2);
+    });
+
+    it('updates just the new HTML for a repeated major change', function() {
+      const page = new Page('1', {changeThreshold: 100,
+                                  state: Page.stateEnum.CHANGED});
+      const html1 = 'Here is some <b>HTML</b>';
+      const html2 = 'Here is some different <b>HTML</b>';
+      spyOn(page, 'save');
+      spyOn(PageStore, 'saveHtml');
+      spyOn(Fuzzy, 'isMajorChange').and.returnValue(true);
+
+      Scan._updatePageState(page, html1, html2);
+
+      expect(page.state).toEqual(Page.stateEnum.CHANGED);
+      expect(PageStore.saveHtml).toHaveBeenCalledWith(
+        '1', PageStore.htmlTypes.NEW, html2);
+      expect(PageStore.saveHtml).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('start', function() {
+    it('does nothing when given an empty page list', function(done) {
+      spyOn(window, 'fetch');
+      spyOn(PageStore, 'loadHtml');
+
+      new Scan([]).start().then(() => {
+        expect(window.fetch).not.toHaveBeenCalled();
+        expect(PageStore.loadHtml).not.toHaveBeenCalled();
+        done();
+      }).catch((error) => done.fail(error));
+    });
+
+    it('Scans a single page', function(done) {
+      const page = new Page('1', {url: 'http://www.example.com/'});
+      const html = 'Some <b>HTML</b>';
+
+      spyOn(window, 'fetch').and.returnValue(
+        Promise.resolve({ok: true, text: () => html}));
+      spyOn(PageStore, 'loadHtml').and.returnValue(Promise.resolve(html));
+
+      new Scan([page]).start().then(() => {
+        expect(window.fetch).toHaveBeenCalledWith(page.url);
+        expect(PageStore.loadHtml).toHaveBeenCalledWith(
+          '1', PageStore.htmlTypes.NEW);
+        expect(page.error).toEqual(false);
+        done();
+      }).catch((error) => done.fail(error));
+    });
+
+    it('Scans multiple pages', function(done) {
+      const pages = [new Page('1', {url: 'http://www.example.com/'}),
+                     new Page('2', {url: 'http://www.example2.com/'}),
+                     new Page('3', {url: 'http://www.example3.com/'})];
+      const html = 'Some <b>HTML</b>';
+
+      spyOn(window, 'fetch').and.returnValue(
+        Promise.resolve({ok: true, text: () => html}));
+      spyOn(PageStore, 'loadHtml').and.returnValue(Promise.resolve(html));
+
+      new Scan(pages).start().then(() => {
+        expect(window.fetch).toHaveBeenCalledWith(pages[0].url);
+        expect(window.fetch).toHaveBeenCalledWith(pages[1].url);
+        expect(window.fetch).toHaveBeenCalledWith(pages[2].url);
+        expect(PageStore.loadHtml).toHaveBeenCalledWith(
+          '1', PageStore.htmlTypes.NEW);
+        expect(PageStore.loadHtml).toHaveBeenCalledWith(
+          '2', PageStore.htmlTypes.NEW);
+        expect(PageStore.loadHtml).toHaveBeenCalledWith(
+          '3', PageStore.htmlTypes.NEW);
+        expect(pages[0].error).toEqual(false);
+        expect(pages[1].error).toEqual(false);
+        expect(pages[2].error).toEqual(false);
+        done();
+      }).catch((error) => done.fail(error));
+    });
+
+    it('Logs and saves HTTP error status codes', function(done) {
+      const page = new Page('1', {title: 'example',
+                                  url: 'http://www.example.com/'});
+
+      spyOn(window, 'fetch').and.returnValue(
+        Promise.resolve({ok: false, status: 404, statusText: 'no such page'}));
+      spyOn(PageStore, 'loadHtml');
+      spyOn(console, 'log').and.callFake((msg) => {
+        expect(msg).toMatch('no such page');
+        console.matched = true;
+      });
+      console.matched = false;
+
+      new Scan([page]).start().then(() => {
+        expect(window.fetch).toHaveBeenCalledWith(page.url);
+        expect(PageStore.loadHtml).not.toHaveBeenCalled();
+        expect(page.error).toEqual(true);
+        expect(page.errorMessage).toEqual('[404] no such page');
+        expect(console.matched).toEqual(true);
+        done();
+      }).catch((error) => done.fail(error));
+    });
+
+    it('Logs and saves network errors', function(done) {
+      const page = new Page('1', {title: 'example',
+                                  url: 'http://www.example.com/'});
+
+      spyOn(window, 'fetch').and.returnValue(
+        Promise.reject('Network error'));
+      spyOn(PageStore, 'loadHtml');
+      spyOn(console, 'log').and.callFake((msg) => {
+        expect(msg).toMatch('Network error');
+        console.matched = true;
+      });
+      console.matched = false;
+
+      new Scan([page]).start().then(() => {
+        expect(window.fetch).toHaveBeenCalledWith(page.url);
+        expect(PageStore.loadHtml).not.toHaveBeenCalled();
+        expect(page.error).toEqual(true);
+        expect(page.errorMessage).toEqual('Network error');
+        expect(console.matched).toEqual(true);
+        done();
+      }).catch((error) => done.fail(error));
+    });
+  });
+});
