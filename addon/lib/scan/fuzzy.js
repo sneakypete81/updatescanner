bf6612d78abd9d08ee76d4ec9f60a27cc449a3de
sneakypete81 <sneakypete81@gmail.com>
2017-01-05 18:44:16 +0000

    Add Fuzzy skeleton code

---
+++ addon/lib/scan/fuzzy.js
@@ -0,0 +1,21 @@
+/* exported Fuzzy */
+
+/**
+ * Class used to scan a list of webpages and test for updates.
+ */
+class Fuzzy {
+  /**
+   * Check if two HTML strings differ by more than a threshold.
+   *
+   * @param {string} html1 - First HTML string for comparison.
+   * @param {string} html2 - Second HTML string for comparison.
+   * @param {integer} changeThreshold - Number of characters that must change to
+   * indicate a major change.
+   *
+   * @returns {boolean} Returns True if the two HTML strings differ by more than
+   * changeThreshold charagers.
+   */
+  static isMajorChange(html1, html2, changeThreshold) {
+    return false;
+  }
+}
