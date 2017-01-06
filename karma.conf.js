8805fe19da4c481907976ad649900cc83ca7ed46
sneakypete81 <sneakypete81@gmail.com>
2017-01-06 11:37:12 +0000

    Add coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -51,13 +51,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      // Generate coverage for all source files
+      'addon/lib/**/*.js': ['coverage'],
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+    reporters: ['dots', 'coverage'],
 
+    coverageReporter: {
+      reporters: [
+        {type: 'html'},
+        {type: 'text'},
+      ],
+    },
     // web server port
     port: 9876,
 
