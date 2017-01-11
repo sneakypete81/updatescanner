070477a2d2e5ecaf8cae853934f838482659a257
sneakypete81 <sneakypete81@gmail.com>
2017-01-11 18:55:12 +0000

    Load config only once at start of day

--- addon/lib/util/config.js
+++ addon/lib/util/config.js
@@ -1,7 +1,7 @@
 /* exported Config */
 
 /**
- * Static functions to access configuration settings.
+ * Class used to access configuration settings.
  */
 class Config {
 
@@ -9,50 +9,89 @@ class Config {
    * @returns {Object} Dictionary of default values for all valid config names.
    * By convention, all config names are lowercase and hythenated.
    */
-  static get _DEFAULT() {
+  static get _DEFAULTS() {
     return {
       'debug': false, // Enable debug mode
     };
   }
 
   /**
-   * Load a configuration setting, returning a sensible default if the setting
-   * doesn't exist.
-   *
-   * @param {string} name - Name of the configuration setting to load.
-   *
-   * @returns {Promise} A Promise that fulfils with the setting value, or is
-   * rejected if the name is invalid or the storage operation fails.
+   * Config constructor.
    */
-  static load(name) {
-    const default_ = Config._DEFAULT[name];
-    if (default_ === undefined) {
-      return Promise.reject('Invalid config name: ' + name);
-    }
+  constructor() {
+    this._data = {};
+  }
 
-    return Storage.load('config-' + name).then((value) => {
-      if (value === undefined) {
-        return default_;
-      } else {
-        return value;
-      }
+  /**
+   * Load the configuration settings from storage.
+   *
+   * @returns {Promise} A Promise that fulfils with this object (for chaining),
+   * or is rejected if the storage operation fails.
+   */
+  load() {
+    return Storage.load('config').then((storageData) => {
+      this._data = storageData;
+      return this;
     });
   }
 
   /**
-   * Save a configuration setting.
-   *
-   * @param {string} name - Name of the configuration setting to save.
-   * @param {string} value - Setting value to save.
+   * Save the configuration settings to storage.
    *
    * @returns {Promise} An empty Promise that fulfils when the save is complete,
-   * or is rejected if the name is invalid or the storage operation fails.
+   * or is rejected if the storage operation fails.
    */
-  static save(name, value) {
-    if (Config._DEFAULT[name] === undefined) {
-      return Promise.reject('Invalid config name: ' + name);
+  save() {
+    return Storage.save('config', this._data);
+  }
+
+  /**
+   * Returns the specified config setting. If the setting was not loaded from
+   * storage, a sensible default value is returned. Throws if the setting name
+   * is unrecognised.
+   *
+   * @param {string} name - Name of the config setting.
+   *
+   * @returns {Object} The requested config setting.
+   */
+  get(name) {
+    if (!Config._DEFAULTS.hasOwnProperty(name)) {
+      throw new InvalidConfigNameError(name);
     }
 
-    return Storage.save('config-' + name, value);
+    if (this._data.hasOwnProperty(name)) {
+      return this._data[name];
+    } else {
+      return Config._DEFAULTS[name];
+    }
+  }
+
+  /**
+   * Sets the specified config setting. Throws if the setting name is
+   * unrecognised. You will need to call save() to write the updated config to
+   * storage.
+   *
+   * @param {string} name - Name of the config setting.
+   * @param {Object} value - New value of the config setting.
+   */
+  set(name, value) {
+    if (!Config._DEFAULTS.hasOwnProperty(name)) {
+      throw new InvalidConfigNameError(name);
+    }
+
+    this._data[name] = value;
   }
 }
+
+/**
+ * Thrown if a method is passed an invalid configuration setting name.
+ *
+ * @param {string} configName - Name of the config setting.
+ */
+function InvalidConfigNameError(configName) {
+  this.name = 'InvalidConfigNameError';
+  this.message = 'Invalid config setting: ' + configName;
+  this.stack = (new Error()).stack;
+}
+InvalidConfigNameError.prototype = Object.create(Error.prototype);
+InvalidConfigNameError.prototype.constructor = InvalidConfigNameError;
--- test/unit/config_spec.js
+++ test/unit/config_spec.js
@@ -1,39 +1,27 @@
-/* global Config */
+/* global Config, InvalidConfigNameError */
 
 describe('Config', function() {
   describe('load', function() {
     it('loads a config setting from storage', function(done) {
-      spyOn(Storage, 'load').and.returnValues(Promise.resolve(true));
+      spyOn(Storage, 'load').and.returnValues(Promise.resolve({debug: true}));
 
-      Config.load('debug').then((result) => {
-        expect(Storage.load).toHaveBeenCalledWith('config-debug');
-        expect(result).toEqual(true);
+      const config = new Config();
+      config.load().then(() => {
+        expect(Storage.load).toHaveBeenCalledWith('config');
+        expect(config.get('debug')).toEqual(true);
         done();
       }).catch((error) => done.fail(error));
     });
 
-    it('returns a default if the setting is not in storage', function(done) {
-      spyOn(Storage, 'load').and.returnValues(Promise.resolve(undefined));
+    it('loads a config setting from storage with chained construction',
+        function(done) {
+      spyOn(Storage, 'load').and.returnValues(Promise.resolve({debug: true}));
 
-      Config.load('debug').then((result) => {
-        expect(Storage.load).toHaveBeenCalledWith('config-debug');
-        // Default for the debug setting is false
-        expect(result).toEqual(false);
+      new Config().load().then((config) => {
+        expect(Storage.load).toHaveBeenCalledWith('config');
+        expect(config.get('debug')).toEqual(true);
         done();
       }).catch((error) => done.fail(error));
-    });
-
-    it('throws if the setting name is invalid', function(done) {
-      spyOn(Storage, 'load').and.returnValues(Promise.resolve());
-
-      Config.load('invalid-name').then((result) => {
-        done.fail('Promise was not rejected.');
-      })
-      .catch((error) => {
-        expect(Storage.load).not.toHaveBeenCalled();
-        expect(error).toEqual('Invalid config name: invalid-name');
-        done();
-      });
     });
   });
 
@@ -41,23 +29,45 @@ describe('Config', function() {
     it('saves a config setting to storage', function(done) {
       spyOn(Storage, 'save').and.returnValues(Promise.resolve());
 
-      Config.save('debug', true).then(() => {
-        expect(Storage.save).toHaveBeenCalledWith('config-debug', true);
+      const config = new Config();
+      config.set('debug', true);
+      config.save().then(() => {
+        expect(Storage.save).toHaveBeenCalledWith('config', {debug: true});
         done();
       }).catch((error) => done.fail(error));
     });
 
-    it('throws if the setting name is invalid', function(done) {
+    it('doesn\'t save unmodified default settings', function(done) {
       spyOn(Storage, 'save').and.returnValues(Promise.resolve());
 
-      Config.save('invalid-name').then(() => {
-        done.fail('Promise was not rejected.');
-      })
-      .catch((error) => {
-        expect(Storage.save).not.toHaveBeenCalled();
-        expect(error).toEqual('Invalid config name: invalid-name');
+      const config = new Config();
+      config.save().then(() => {
+        // Check that a default value is returned, but isn't saved to storage.
+        expect(config.get('debug')).toEqual(false);
+        expect(Storage.save).toHaveBeenCalledWith('config', {});
         done();
-      });
+      }).catch((error) => done.fail(error));
+    });
+  });
+
+  describe('get', function() {
+    it('returns the default if the setting is not in storage', function() {
+      const result = new Config().get('debug');
+
+      // Default for the debug setting is false.
+      expect(result).toEqual(false);
+    });
+
+    it('throws if the setting name is invalid', function() {
+      expect(() => new Config().get('invalid-setting'))
+        .toThrowError(InvalidConfigNameError);
+    });
+  });
+
+  describe('set', function() {
+    it('throws if the setting name is invalid', function() {
+      expect(() => new Config().set('invalid-setting', 4))
+        .toThrowError(InvalidConfigNameError);
     });
   });
 });
