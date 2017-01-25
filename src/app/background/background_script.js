bccf48f989f52e7ca5c71486290d92a626164924
sneakypete81 <sneakypete81@gmail.com>
2017-01-25 19:21:54 +0000

    Remove unused toolbar button handler

--- src/app/background/background_script.js
+++ src/app/background/background_script.js
@@ -1,14 +1,5 @@
 import * as autoscan from 'scan/autoscan';
 
-// Handle the toolbar button click event
-(function() {
-  chrome.browserAction.onClicked.addListener(() => {
-    const url = chrome.extension.getURL('/app/main/main.html');
-    // chrome.tabs.update({url: url})
-    chrome.tabs.create({url: url});
-  });
-})();
-
 // Run the Autoscan process in the background
 (function() {
   autoscan.start();
