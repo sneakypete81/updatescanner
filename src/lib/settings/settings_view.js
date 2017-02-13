10fde96837d13f2fdfb75cb4c8003c5df685798c
sneakypete81 <sneakypete81@gmail.com>
2017-02-13 12:53:51 +0000

    Update slider description text dynamically

--- src/lib/settings/settings_view.js
+++ src/lib/settings/settings_view.js
@@ -1,61 +1,4 @@
-import {qs} from 'util/view_helpers';
-
-const AutoscanSliderToMins = [
-  5,
-  15,
-  30,
-  60,
-  6 * 60,
-  24 * 60,
-  7 * 24 * 60,
-  0, // Never
-];
-const AutoscanSliderNever = AutoscanSliderToMins.indexOf(0);
-
-/**
- * @param {integer} minutes - Number of minutes between scans.
- *
- * @returns {integer} Slider value representing the given number of minutes.
- */
-function autoscanMinsToSlider(minutes) {
-  if (minutes == 0) {
-    return AutoscanSliderNever;
-  }
-
-  // Walk through the options, returning the first one that matches
-  for (let i=0; i<AutoscanSliderToMins.length; i++) {
-    if (AutoscanSliderToMins[i] >= minutes) {
-      return i;
-    }
-  }
-
-  // Round down to 7 weeks
-  return AutoscanSliderNever - 1;
-}
-
-const ThresholdSliderToChars = [
-  0,
-  10,
-  50,
-  100,
-  500,
-  1000,
-];
-
-/**
- * @param {integer} changeThreshold - Change threshold measured in characters.
- *
- * @returns {integer} Slider value representing the given number of characters.
- */
-function thresholdCharsToSlider(changeThreshold) {
-  // Walk through the options, returning the first one that matches
-  for (let i=0; i<ThresholdSliderToChars.length; i++) {
-    if (ThresholdSliderToChars[i] >= changeThreshold) {
-      return i;
-    }
-  }
-  return thresholdCharsToSlider.length - 1;
-}
+import {qs, $on} from 'util/view_helpers';
 
 /**
  * Initialise the Page Settings view.
@@ -64,6 +7,11 @@ export function init() {
   const form = qs('#form');
   form.autoscan.max = AutoscanSliderToMins.length - 1;
   form.threshold.max = ThresholdSliderToChars.length - 1;
+
+  $on(form.autoscan, 'input', ({target}) =>
+    updateAutoscanDescription(target.value));
+  $on(form.threshold, 'input', ({target}) =>
+    updateThresholdDescription(target.value));
 }
 
 /**
@@ -89,8 +37,93 @@ export function update({title, url, scanRateMinutes, changeThreshold}) {
   }
   if (scanRateMinutes !== undefined) {
     form.autoscan.value = autoscanMinsToSlider(scanRateMinutes);
+    updateAutoscanDescription(form.autoscan.value);
   }
   if (changeThreshold !== undefined) {
     form.threshold.value = thresholdCharsToSlider(changeThreshold);
+    updateThresholdDescription(form.threshold.value);
   }
+}
+
+const AutoscanSliderMap = new Map([
+  [5, 'Scan every 5 minutes'],
+  [15, 'Scan every 15 minutes'],
+  [30, 'Scan every 30 minutes'],
+  [60, 'Scan every hour'],
+  [6 * 60, 'Scan every 6 hours'],
+  [24 * 60, 'Scan every day'],
+  [7 * 24 * 60, 'Scan every week'],
+  [0, 'Manual scan only'],
+]);
+const AutoscanSliderToMins = [...AutoscanSliderMap.keys()];
+const AutoscanSliderDescriptions = [...AutoscanSliderMap.values()];
+const AutoscanSliderNever = AutoscanSliderToMins.indexOf(0);
+
+/**
+ * @param {integer} minutes - Number of minutes between scans.
+ *
+ * @returns {integer} Slider value representing the given number of minutes.
+ */
+function autoscanMinsToSlider(minutes) {
+  if (minutes == 0) {
+    return AutoscanSliderNever;
+  }
+
+  // Walk through the options, returning the first one that matches
+  for (let i=0; i<AutoscanSliderToMins.length; i++) {
+    if (AutoscanSliderToMins[i] >= minutes) {
+      return i;
+    }
+  }
+
+  // Round down to 7 weeks
+  return AutoscanSliderNever - 1;
+}
+
+/**
+ * Update the Autoscan description text based on the current slider value.
+ *
+ * @param {integer} sliderValue - Autoscan slider value.
+ */
+function updateAutoscanDescription(sliderValue) {
+  qs('#form').elements['autoscan-description'].value =
+    AutoscanSliderDescriptions[sliderValue];
+}
+
+const ThresholdSliderMap = new Map([
+  [0, ['All changes are detected', '']],
+  [10, ['Cosmetic changes are ignored', '(less than about 10 characters)']],
+  [50, ['Minor changes are ignored', '(less than about 50 characters)']],
+  [100, ['Small changes are ignored', '(less than about 100 characters)']],
+  [500, ['Medium changes are ignored', '(less than about 500 characters)']],
+  [1000, ['Major changes are ignored', '(less than about 1000 characters)']],
+]);
+const ThresholdSliderToChars = [...ThresholdSliderMap.keys()];
+const ThresholdSliderDescriptions = [...ThresholdSliderMap.values()];
+
+/**
+ * @param {integer} changeThreshold - Change threshold measured in characters.
+ *
+ * @returns {integer} Slider value representing the given number of characters.
+ */
+function thresholdCharsToSlider(changeThreshold) {
+  // Walk through the options, returning the first one that matches
+  for (let i=0; i<ThresholdSliderToChars.length; i++) {
+    if (ThresholdSliderToChars[i] >= changeThreshold) {
+      return i;
+    }
+  }
+  return thresholdCharsToSlider.length - 1;
+}
+
+/**
+ * Update the Threshold description text based on the current slider value.
+ *
+ * @param {integer} sliderValue - Threshold slider value.
+ */
+function updateThresholdDescription(sliderValue) {
+  qs('#form').elements['threshold-description'].value =
+    ThresholdSliderDescriptions[sliderValue][0];
+  qs('#form').elements['threshold-subdescription'].value =
+    ThresholdSliderDescriptions[sliderValue][1];
 }
