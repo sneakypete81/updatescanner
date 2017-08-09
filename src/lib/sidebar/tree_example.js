0b4a9a51846f01edea388d247a08de0ca40f73a7
sneakypete81 <sneakypete81@gmail.com>
2017-08-09 21:09:01 +0100

    Linter fixes

--- src/lib/sidebar/tree_example.js
+++ src/lib/sidebar/tree_example.js
@@ -5,64 +5,64 @@ export const tree = {
     collapsed: true,
     children: [{
       module: 'node.js',
-      leaf: true
+      leaf: true,
     }, {
       module: 'react-ui-tree.css',
-      leaf: true
+      leaf: true,
     }, {
       module: 'react-ui-tree.js',
-      leaf: true
+      leaf: true,
     }, {
       module: 'tree.js',
-      leaf: true
-    }]
+      leaf: true,
+    }],
   }, {
     module: 'example',
     children: [{
       module: 'app.js',
-      leaf: true
+      leaf: true,
     }, {
       module: 'app.less',
-      leaf: true
+      leaf: true,
     }, {
       module: 'index.html',
-      leaf: true
-    }]
+      leaf: true,
+    }],
   }, {
     module: 'lib',
     children: [{
       module: 'node.js',
-      leaf: true
+      leaf: true,
     }, {
       module: 'react-ui-tree.js',
-      leaf: true
+      leaf: true,
     }, {
       module: 'react-ui-tree.less',
-      leaf: true
+      leaf: true,
     }, {
       module: 'tree.js',
-      leaf: true
-    }]
+      leaf: true,
+    }],
   }, {
     module: '.gitiignore',
-    leaf: true
+    leaf: true,
   }, {
     module: 'index.js',
-    leaf: true
+    leaf: true,
   }, {
     module: 'LICENSE',
-    leaf: true
+    leaf: true,
   }, {
     module: 'Makefile',
-    leaf: true
+    leaf: true,
   }, {
     module: 'package.json',
-    leaf: true
+    leaf: true,
   }, {
     module: 'README.md',
-    leaf: true
+    leaf: true,
   }, {
     module: 'webpack.config.js',
-    leaf: true
-  }]
-}
+    leaf: true,
+  }],
+};
