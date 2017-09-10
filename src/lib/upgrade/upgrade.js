c77cd0af99bedf7551827ceb39dabd8d89c879df
sneakypete81 <sneakypete81@gmail.com>
2017-09-10 19:38:49 +0100

    Find the Update Scanner root bookmark folder

--- src/lib/upgrade/upgrade.js
+++ src/lib/upgrade/upgrade.js
@@ -12,7 +12,8 @@ export class Upgrade {
     $on(qs('#loadButton'), 'click', async (event) => {
       const file = qs('#fileInput').files[0];
       const bookmarks = await loadBookmarks(file);
-      importPages(bookmarks);
+      const root = findRoot(bookmarks.children);
+      importPages(root);
     });
   }
 }
@@ -34,10 +35,37 @@ async function loadBookmarks(file) {
 }
 
 /**
+ * Find the Update Scanner root bookmark folder.
+ *
+ * @param {Object} bookmarks - Bookmark object to search.
+ * @returns {Object} Root Bookmark folder.
+ */
+function findRoot(bookmarks) {
+  for (let bookmarkIdx=0; bookmarkIdx < bookmarks.length; bookmarkIdx++) {
+    const bookmark = bookmarks[bookmarkIdx];
+    if (bookmark.hasOwnProperty('annos')) {
+      for (let annoIdx=0; annoIdx < bookmark.annos.length; annoIdx++) {
+        const anno = bookmark.annos[annoIdx];
+        if (anno.name == 'updatescan/root') {
+          return bookmark;
+        }
+      }
+    }
+
+    if (bookmark.hasOwnProperty('children')) {
+      const result = findRoot(bookmark.children);
+      if (result !== undefined) {
+        return result;
+      }
+    }
+  }
+  return undefined;
+}
+/**
  * Import Update Scanner pages from a bookmarks object.
  *
- * @param {Object} bookmarks - Bookmark object to import pages from.
+ * @param {Object} root - Update Scanner root bookmarks object.
  */
-function importPages(bookmarks) {
-  console.log(bookmarks);
+function importPages(root) {
+  console.log(root);
 }
