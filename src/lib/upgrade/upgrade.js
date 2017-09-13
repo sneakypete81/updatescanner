d57e66a8d663fba1f70f6b016ae9a5f0680215d2
sneakypete81 <sneakypete81@gmail.com>
2017-09-13 19:42:47 +0100

    Add code to import pages from bookmarks

--- src/lib/upgrade/upgrade.js
+++ src/lib/upgrade/upgrade.js
@@ -1,5 +1,6 @@
 import {qs, $on} from 'util/view_helpers';
 import {readAsText} from 'promise-file-reader';
+import {PageStore} from 'page/page_store';
 
 /**
  * Class representing the Update Scanner upgrade page.
@@ -13,7 +14,9 @@ export class Upgrade {
       const file = qs('#fileInput').files[0];
       const bookmarks = await loadBookmarks(file);
       const root = findRoot(bookmarks.children);
-      importPages(root);
+
+      const pageStore = await PageStore.load();
+      importPages(pageStore, root);
     });
   }
 }
@@ -61,11 +64,41 @@ function findRoot(bookmarks) {
   }
   return undefined;
 }
+
 /**
  * Import Update Scanner pages from a bookmarks object.
  *
+ * @param {PageStore} pageStore - PageStore to import bookmarks into..
  * @param {Object} root - Update Scanner root bookmarks object.
  */
-function importPages(root) {
-  console.log(root);
+async function importPages(pageStore, root) {
+  root.children.forEach(async (child) => {
+    if (child.hasOwnProperty('children')) {
+      importPages(pageStore, child);
+    } else {
+      const annos = extractAnnos(child);
+      const page = await pageStore.createPage(PageStore.ROOT_ID);
+      page.title = child.title;
+      page.url = child.uri;
+      page.scanRateMinutes = annos['updatescan/scan_rate_mins'];
+      page.changeThreshold = annos['updatescan/threshold'];
+      page.ignoreNumbers = (annos['updatescan/ignore_numbers'] == 1);
+      page.save();
+    }
+  });
+}
+
+/**
+ * Extract a bookmark's annotations into an object.
+ *
+ * @param {Object} bookmark - Bookmark object to process.
+ * @returns {Object} Object containing the bookmark's annotations.
+ */
+function extractAnnos(bookmark) {
+  let annos = {};
+  bookmark.annos.forEach((anno) => {
+    annos[anno.name] = anno.value;
+  });
+
+  return annos;
 }
