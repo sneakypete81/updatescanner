19d39f147a46ab25625538ddd64d14d9d4f1b3fb
sneakypete81 <sneakypete81@gmail.com>
2017-09-16 19:55:01 +0100

    Wait 200ms before performing a refresh

--- src/lib/sidebar/sidebar.js
+++ src/lib/sidebar/sidebar.js
@@ -3,6 +3,9 @@ import {PageStore} from 'page/page_store';
 import {Page} from 'page/page';
 import {openMain, paramEnum, actionEnum} from 'main/main_url';
 
+const REFRESH_ALARM_ID = 'updatescanner-sidebar-refresh';
+const REFRESH_ALARM_TIMING = {delayInMinutes: 0.2 / 60};
+
 /**
  * Class representing the main Update Scanner content page.
  */
@@ -18,7 +21,16 @@ export class Sidebar {
     this.sidebar = new SidebarView('#tree');
     this.pageStore = null;
     this.currentPage = null;
+    this.isRefreshPending = false;
     this.isRefreshing = false;
+
+    // Refresh whenever the alarm goes off
+    browser.alarms.onAlarm.addListener((alarm) => {
+      if (alarm.name == REFRESH_ALARM_ID) {
+        this.isRefreshPending = false;
+        this._refreshSidebar();
+      }
+    });
   }
 
   /**
@@ -73,6 +85,9 @@ export class Sidebar {
    * @param {storage.StorageChange} change - Object representing the change.
    */
   _handlePageUpdate(pageId, change) {
-    this._refreshSidebar();
+    if (!this.isRefreshPending) {
+      this.isRefreshPending = true;
+      browser.alarms.create(REFRESH_ALARM_ID, REFRESH_ALARM_TIMING);
+    }
   }
 }
