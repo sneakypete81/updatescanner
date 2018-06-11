730bc78c2cc6e60915e8eb7bdd1a78ac31f6360e
sneakypete81 <sneakypete81@gmail.com>
2018-06-11 21:00:55 +0100

    Don't lint dependencies

--- grunt/eslint.js
+++ grunt/eslint.js
@@ -1,6 +1,12 @@
 module.exports = {
   options: {
-    ignorePattern: ['node_modules/', 'coverage/', 'build/', 'src/dependencies'],
+    ignorePattern: [
+      'node_modules/',
+      'coverage/',
+      'build/',
+      'src/dependencies',
+      'test/dependencies',
+    ],
     maxWarnings: 0,
   },
   target: ['.'],
