8098062dc0f4e70b469c9612b3c60b918953c38a
sneakypete81 <sneakypete81@gmail.com>
2018-07-28 20:37:05 +0100

    Refactor Karma script

--- scripts/test.js
+++ scripts/test.js
@@ -3,10 +3,4 @@ require('./copy-dependencies');
 
 console.log('Running tests...');
 
-const Server = require('karma').Server;
-
-const server = new Server({
-  configFile: __dirname + '/../karma.conf.js',
-});
-
-server.start();
+require('./karma').run();
