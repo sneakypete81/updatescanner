e5df17cccfd62d8597083e01e3c48e94750ff11c
sneakypete81 <sneakypete81@gmail.com>
2019-05-29 10:09:01 +0100

    Remove functional tests for now

--- scripts/lib/wdio.js
+++
@@ -1,7 +0,0 @@
-exports.run = async function() {
-  const Launcher = require('@wdio/cli').default;
-  const wdio = new Launcher(
-    __dirname + '/../../test/functional/wdio.conf.js', {});
-
-  return await wdio.run();
-};
--- test/functional/pages/chrome.page.js
+++
@@ -1,7 +0,0 @@
-class ChromePage {
-  get updateScannerButton() {
-    return $('#_c07d1a49-9894-49ff-a594-38960ede8fb9_-browser-action');
-  }
-}
-
-module.exports = new ChromePage();
