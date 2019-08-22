dbd4bbe69127cdc85187422ef3445cd3ece5ef02
sneakypete81 <sneakypete81@gmail.com>
2019-08-22 19:13:37 +0100

    Refactor to simplify pages.js

--- src/lib/redux/ducks/pages.js
+++ src/lib/redux/ducks/pages.js
@@ -4,25 +4,48 @@ const DELETE_ITEM = 'pages/DELETE_ITEM';
 const EDIT_PAGE = 'pages/EDIT_PAGE';
 const EDIT_FOLDER = 'pages/EDIT_FOLDER';
 
-const addPage = (state, action) => {
+const initialState = {0: {title: 'root', children: []}};
+
+/**
+ * @param {object} state - Current state.
+ * @param {object} action - Action to apply.
+ * @returns {object} - New state.
+ */
+export default function reducer(state=initialState, action) {
+  const actionHandlers = {
+    [ADD_PAGE]: handleAddPage,
+    [ADD_FOLDER]: handleAddFolder,
+    [DELETE_ITEM]: handleDeleteItem,
+    [EDIT_PAGE]: handleEditPage,
+    [EDIT_FOLDER]: handleEditFolder,
+  };
+
+  const actionHandler = actionHandlers[action.type];
+  if (actionHandler === undefined) {
+    return state;
+  }
+  return actionHandler(state, action);
+}
+
+const handleAddPage = (state, action) => {
   const id = getNextId(state);
   return {
-    ...addChild(state, action.payload.parentId, id),
-    [id]: action.payload.page,
+    ...addChild(state, action.parentId, id),
+    [id]: action.page,
   };
 };
 
-const addFolder = (state, action) => {
+const handleAddFolder = (state, action) => {
   const id = getNextId(state);
-  const newFolder = {...action.payload.folder, children: []};
+  const newFolder = {...action.folder, children: []};
   return {
-    ...addChild(state, action.payload.parentId, id),
+    ...addChild(state, action.parentId, id),
     [id]: newFolder,
   };
 };
 
-const deleteItem = (state, action) => {
-  const id = action.payload.id;
+const handleDeleteItem = (state, action) => {
+  const id = action.id;
   const parentId = findParentId(state, id);
   const newState = removeChild(state, parentId, id);
 
@@ -30,17 +53,17 @@ const deleteItem = (state, action) => {
   return newState;
 };
 
-const editPage = (state, action) => {
-  const id = action.payload.id;
-  const newPage = {...state[id], ...action.payload.page};
+const handleEditPage = (state, action) => {
+  const id = action.id;
+  const newPage = {...state[id], ...action.page};
   return {...state, [id]: newPage};
 };
 
-const editFolder = (state, action) => {
-  const id = action.payload.id;
+const handleEditFolder = (state, action) => {
+  const id = action.id;
   const newPage = {
     ...state[id],
-    ...action.payload.folder,
+    ...action.folder,
     children: state[id].children,
   };
   return {...state, [id]: newPage};
@@ -79,26 +102,3 @@ const getFolderIds = (state) =>
   Object.keys(state).filter(
     (id) => Object.prototype.hasOwnProperty.call(state[id], 'children')
   );
-
-
-const initialState = {0: {title: 'root', children: []}};
-
-const pages = {
-  reducer: (state=initialState, action) => {
-    const actionHandlers = {
-      [ADD_PAGE]: addPage,
-      [ADD_FOLDER]: addFolder,
-      [DELETE_ITEM]: deleteItem,
-      [EDIT_PAGE]: editPage,
-      [EDIT_FOLDER]: editFolder,
-    };
-
-    const actionHandler = actionHandlers[action.type];
-    if (actionHandler === undefined) {
-      return state;
-    }
-    return actionHandler(state, action);
-  },
-};
-
-export {pages as default};
