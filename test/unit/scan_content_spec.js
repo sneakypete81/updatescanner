473dfa46a4c3190c61ff1a668cb16213c8a73fa7
adsamcik <adsamcik@users.noreply.github.com>
2020-01-07 13:51:34 +0100

    Renamed import

--- test/unit/scan_content_spec.js
+++ test/unit/scan_content_spec.js
@@ -1,4 +1,4 @@
-import * as scanModule from '/lib/scan/scan_content.js';
+import * as scanContentModule from '/lib/scan/scan_content.js';
 import {Page} from '/lib/page/page.js';
 
 describe('scan_content', function() {
@@ -6,37 +6,37 @@ describe('scan_content', function() {
     it('detects identical pages', function() {
       const html = 'Here is some <b>HTML</b>';
 
-      const result = scanModule.__.getChangeType(html, html, 100);
+      const result = scanContentModule.__.getChangeType(html, html, 100);
 
-      expect(result).toEqual(scanModule.__.changeEnum.NO_CHANGE);
+      expect(result).toEqual(scanContentModule.__.changeEnum.NO_CHANGE);
     });
 
     it('detects new content', function() {
       const html = 'Here is some <b>HTML</b>';
 
-      const result = scanModule.__.getChangeType(null, html, 100);
+      const result = scanContentModule.__.getChangeType(null, html, 100);
 
-      expect(result).toEqual(scanModule.__.changeEnum.NEW_CONTENT);
+      expect(result).toEqual(scanContentModule.__.changeEnum.NEW_CONTENT);
     });
 
     it('detects minor changes', function() {
       const html1 = 'Here is some <b>HTML</b>';
       const html2 = 'Here is some different <b>HTML</b>';
-      spyOn(scanModule.__, 'isMajorChange').and.returnValues(false);
+      spyOn(scanContentModule.__, 'isMajorChange').and.returnValues(false);
 
-      const result = scanModule.__.getChangeType(html1, html2, 100);
+      const result = scanContentModule.__.getChangeType(html1, html2, 100);
 
-      expect(result).toEqual(scanModule.__.changeEnum.MINOR_CHANGE);
+      expect(result).toEqual(scanContentModule.__.changeEnum.MINOR_CHANGE);
     });
 
     it('detects major changes', function() {
       const html1 = 'Here is some <b>HTML</b>';
       const html2 = 'Here is some different <b>HTML</b>';
-      spyOn(scanModule.__, 'isMajorChange').and.returnValues(true);
+      spyOn(scanContentModule.__, 'isMajorChange').and.returnValues(true);
 
-      const result = scanModule.__.getChangeType(html1, html2, 100);
+      const result = scanContentModule.__.getChangeType(html1, html2, 100);
 
-      expect(result).toEqual(scanModule.__.changeEnum.MAJOR_CHANGE);
+      expect(result).toEqual(scanContentModule.__.changeEnum.MAJOR_CHANGE);
     });
   });
 
@@ -45,8 +45,16 @@ describe('scan_content', function() {
       const prevHtml = 'text with\tspaces\ntabs  \n \r  and newlines';
       const scannedHtml = 'More text with\tspaces\ntabs  \n \r  and newlines';
 
-      const resultPrevHtml = scanModule.__.stripHtml(prevHtml, false, false);
-      const resultScanHtml = scanModule.__.stripHtml(scannedHtml, false, false);
+      const resultPrevHtml = scanContentModule.__.stripHtml(
+        prevHtml,
+        false,
+        false,
+      );
+      const resultScanHtml = scanContentModule.__.stripHtml(
+        scannedHtml,
+        false,
+        false,
+      );
 
       expect(resultPrevHtml).toEqual('textwithspacestabsandnewlines');
       expect(resultScanHtml).toEqual('Moretextwithspacestabsandnewlines');
@@ -58,7 +66,7 @@ describe('scan_content', function() {
       const scannedHtml = 'text with <script>\nnewlines</script> of various' +
         '<script>\r\n   types\r\n</script>..';
 
-      const result = scanModule.__.stripHtml(
+      const result = scanContentModule.__.stripHtml(
         prevHtml,
         scannedHtml,
         true,
@@ -73,7 +81,7 @@ describe('scan_content', function() {
       const prevHtml = 'text with <b>tags</b> included.';
       const scannedHtml = '<p>More text with<br/>tags</p>';
 
-      const result = scanModule.__.stripHtml(
+      const result = scanContentModule.__.stripHtml(
         prevHtml,
         scannedHtml,
         true,
@@ -88,7 +96,12 @@ describe('scan_content', function() {
       const prevHtml = 'text with 12.3 numbers, full stops and commas.';
       const scannedHtml = 'More text with 12.3 numbers, etc.';
 
-      const result = scanModule.__.stripHtml(prevHtml, scannedHtml, true, true);
+      const result = scanContentModule.__.stripHtml(
+        prevHtml,
+        scannedHtml,
+        true,
+        true,
+      );
 
       expect(result.prevHtml).toEqual('textwithnumbersfullstopsandcommas');
       expect(result.scannedHtml).toEqual('Moretextwithnumbersetc');
@@ -98,14 +111,18 @@ describe('scan_content', function() {
       const prevHtml = 'text with 12.3 numbers, stops and commas.';
       const scannedHtml = 'More text with 12.3 numbers, etc.';
 
-      const result = scanModule.__.stripHtml(prevHtml, scannedHtml, false);
+      const result = scanContentModule.__.stripHtml(
+        prevHtml,
+        scannedHtml,
+        false,
+      );
 
       expect(result.prevHtml).toEqual('textwith12.3numbers,stopsandcommas.');
       expect(result.scannedHtml).toEqual('Moretextwith12.3numbers,etc.');
     });
 
     it('handles null HTML input', function() {
-      const result = scanModule.__.stripHtml(null, null, true);
+      const result = scanContentModule.__.stripHtml(null, null, true);
 
       expect(result.prevHtml).toBe(null);
       expect(result.scannedHtml).toBe(null);
@@ -116,12 +133,15 @@ describe('scan_content', function() {
     it('applies the encoding in the Page object', async function() {
       const page = new Page(1, {encoding: 'encoding'});
       const response = {arrayBuffer: () => Promise.resolve('buffer')};
-      spyOn(scanModule.__, 'applyEncoding').and.returnValue('html');
+      spyOn(scanContentModule.__, 'applyEncoding').and.returnValue('html');
 
-      const html = await scanModule.__.getHtmlFromResponse(response, page);
+      const html = await scanContentModule.__.getHtmlFromResponse(
+        response,
+        page,
+      );
 
       expect(html).toEqual('html');
-      expect(scanModule.__.applyEncoding)
+      expect(scanContentModule.__.applyEncoding)
         .toHaveBeenCalledWith('buffer', 'encoding');
     });
 
@@ -133,19 +153,25 @@ describe('scan_content', function() {
           arrayBuffer: () => Promise.resolve('buffer'),
           headers: 'headers',
         };
-        spyOn(scanModule.__, 'detectEncoding').and.returnValue('encoding');
-        spyOn(scanModule.__, 'applyEncoding').and.returnValue('html');
+        spyOn(
+          scanContentModule.__,
+          'detectEncoding',
+        ).and.returnValue('encoding');
+        spyOn(scanContentModule.__, 'applyEncoding').and.returnValue('html');
         spyOn(Page, 'load').and.returnValue(Promise.resolve(page));
         spyOn(page, 'save');
 
-        const html = await scanModule.__.getHtmlFromResponse(response, page);
+        const html = await scanContentModule.__.getHtmlFromResponse(
+          response,
+          page,
+        );
 
         expect(html).toEqual('html');
-        expect(scanModule.__.applyEncoding)
+        expect(scanContentModule.__.applyEncoding)
           .toHaveBeenCalledWith('buffer', 'utf-8');
-        expect(scanModule.__.detectEncoding)
+        expect(scanContentModule.__.detectEncoding)
           .toHaveBeenCalledWith('headers', 'html');
-        expect(scanModule.__.applyEncoding)
+        expect(scanContentModule.__.applyEncoding)
           .toHaveBeenCalledWith('buffer', 'encoding');
         expect(page.save).toHaveBeenCalled;
         expect(page.encoding).toEqual('encoding');
@@ -156,7 +182,10 @@ describe('scan_content', function() {
       const page = new Page(1, {encoding: 'utf-8'});
       const response = {text: () => Promise.resolve('html')};
 
-      const html = await scanModule.__.getHtmlFromResponse(response, page);
+      const html = await scanContentModule.__.getHtmlFromResponse(
+        response,
+        page,
+      );
 
       expect(html).toEqual('html');
     });
