d4f7b9f0bf0c4010f49a108f37ab0cdea7bd115a
sneakypete81 <sneakypete81@gmail.com>
2020-01-26 19:24:09 +0000

    Use karma-notify-reporter

--- scripts/test-watch.js
+++ scripts/test-watch.js
@@ -3,12 +3,8 @@ require('./copy-dependencies');
 
 console.log('Running tests...');
 
-const isWindows = /^win/.test(process.platform);
-
 require('./lib/karma').run({
   singleRun: false,
   autoWatch: true,
-  // Windows is not supported by the notification reporter
-  reporters: ['dots', 'kjhtml'].concat(
-    isWindows ? [] : ['notification']),
+  reporters: ['dots', 'kjhtml', 'notify'],
 });
